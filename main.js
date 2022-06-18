75571bf4a05bfb46d907c746c3a31fcf0a09236c
AmosJune <amos.sifuma@student.moringaschool.com.com>
2022-06-18 11:28:48 +0300

    Done.

--- main.js
+++ main.js
@@ -3,6 +3,33 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const likeHeart = document.querySelectorAll('.like-glyph');
+
+for(const glyph of likeHeart){
+  glyph.addEventListener('click', likeCallHeart)
+};
+
+function likeCallHeart(event){
+  const heart = event.target;
+  event.preventDefault();
+  mimicServerCall() //Acts as fetch
+  .then(function(){
+    if(heart.textContent === EMPTY_HEART){
+      heart.textContent = FULL_HEART;
+      heart.className = 'activated-heart'
+    }
+    else{
+      event.target.innerText = EMPTY_HEART,
+      event.target.className = ` `;
+    }
+  })
+  .catch(function(){
+    const modal = document.getElementById('modal');
+    modal.className = " ";
+    modal.innerHTML = error;
+    setTimeout(() => modal.className = 'hidden', 3000)
+  });
+};
 
 
 
